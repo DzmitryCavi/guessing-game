b86c72e0c7478f7e57d54316c19b050a14ad5f8c
Dzmitry Malakhouski <dzmitry.malakhouski@tut.by>
2019-10-07 16:04:49 +0300

    feat: add methods for class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0;
+        this.right = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.right + this.left)/2);
+        return this.mid;
+        
     }
 
     lower() {
-
+        this.right = this.mid;
     }
 
     greater() {
-
+        this.left = this.mid;
     }
 }
-
 module.exports = GuessingGame;
